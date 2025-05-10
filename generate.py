3c95b38e70a2660d4e5b0ab30217af69ec01b758
MaxLSB <m.lasbordes@gmail.com>
2025-05-10 13:11:38 +0200

    update

--- generate.py
+++ generate.py
@@ -101,7 +101,7 @@ async def generate_batches(
                         client,
                         msgs,
                         model=model_name,
-                        temperature=0.9,
+                        temperature=0.8,
                         max_tokens=512,
                         top_p=0.95,
                     )
